acdb5dd610a1ccc4519164aee6df2b72eefd3d28
Thiago Araujo <thiagoaraujo.tec@gmail.com>
2026-08-08 10:56:43 -0300

    Updates

---
+++ server/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    if (response.status !== 404 || request.method !== 'GET') return response;
+
+    const indexUrl = new URL('/index.html', request.url);
+    return env.ASSETS.fetch(new Request(indexUrl, request));
+  },
+};
